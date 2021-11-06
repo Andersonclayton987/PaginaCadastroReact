afd28b33cbe0c5646cfd33a8d5f048a609281f99
Andersonclayton987 <andersonclayton987@gmail.com>
2021-11-06 00:39:29 -0300

    Primeiro Commit

---
+++ crud/frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início" subtitle="Cadastro de usuários em React">
+        <div className="display-4">Bem Vindo!</div>
+        <hr />
+        <p className="mb-0"> Sistema criado em React com cadastramento de usuários</p>
+    </Main>
---
+++ crud/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i class="fab fa-react"></i> por <strong>Anderson</strong>
+        </span>
+    </footer>
